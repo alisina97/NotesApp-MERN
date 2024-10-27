9e03abda8c40914b9943b797b39f13fcb792c1e2
Ali Sina <ali.sina1447@gmail.com>
2024-10-27 15:18:32 -0400

    ADD - Login page UI

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E"
+      }
+    },
   },
   plugins: [],
 }
