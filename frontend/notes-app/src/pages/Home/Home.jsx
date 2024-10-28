2ab5e3765953d94810f8f401a391c2bef9165bd5
Ali Sina <ali.sina1447@gmail.com>
2024-10-28 19:46:34 -0400

    ADD : Note card added

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
 
 function Home() {
   return (
     <>
     <Navbar/>
-    
+    <div className='container mx-auto'>
+      <NoteCard 
+      title={"test"} 
+      date={"2023-01-01"} 
+      content={"content long test content long test content long test content long test content long test content long test"} 
+      tags="tag 1"
+      isPinned={true}
+      onEdit={() => {}}
+      onDelete={() => {}}
+      onPinNote={() => {}}
+      />
+    </div>
     </>
   )
 }
