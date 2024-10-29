e0b2ea057be10cfd3239d7694ebc7aa372c2f12d
Ali Sina <ali.sina1447@gmail.com>
2024-10-29 01:23:51 -0400

    UPDATE : styling of Note card & Add Note button

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,22 +1,58 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
 
 function Home() {
   return (
     <>
     <Navbar/>
     <div className='container mx-auto'>
-      <NoteCard 
-      title={"test"} 
-      date={"2023-01-01"} 
-      content={"content long test content long test content long test content long test content long test content long test"} 
-      tags="tag 1"
-      isPinned={true}
-      onEdit={() => {}}
-      onDelete={() => {}}
-      onPinNote={() => {}}
-      />
+      <div className='grid grid-cols-3 gap-4 mt-8'>
+        <NoteCard 
+        title={"Meeting on April 7th"} 
+        date={"3rd April 2024"} 
+        content={"content long test content long test content long test content long test content long test content long test"} 
+        tags="#Meeting"
+        isPinned={false}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        onPinNote={() => {}}
+        />
+                <NoteCard 
+        title={"Meeting on April 7th"} 
+        date={"3rd April 2024"} 
+        content={"content long test content long test content long test content long test content long test content long test"} 
+        tags="#Meeting"
+        isPinned={false}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        onPinNote={() => {}}
+        />
+                <NoteCard 
+        title={"Meeting on April 7th"} 
+        date={"3rd April 2024"} 
+        content={"content long test content long test content long test content long test content long test content long test"} 
+        tags="#Meeting"
+        isPinned={false}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        onPinNote={() => {}}
+        />
+                <NoteCard 
+        title={"Meeting on April 7th"} 
+        date={"3rd April 2024"} 
+        content={"content long test content long test content long test content long test content long test content long test"} 
+        tags="#Meeting"
+        isPinned={false}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        onPinNote={() => {}}
+        />
+      </div>
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' onClick={() => {}}>
+        <MdAdd className='text-[32px] text-white'/>
+      </button>
     </div>
     </>
   )
