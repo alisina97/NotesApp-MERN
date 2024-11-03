08e6fd97ffbf0fe85f5405196c3b8269c485487d
Ali Sina <ali.sina1447@gmail.com>
2024-11-03 10:49:40 -0500

    ADD : get-all-notes endpoint

--- backend/index.js
+++ backend/index.js
@@ -188,6 +188,26 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
     
 })
 
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+    const {user} = req.user;
+    
+    try {
+        const notes = await Note.find({userId: user._id}).sort({isPinned: -1});
+
+        return res.json({
+            error: false,
+            notes,
+            message: "All notes recieved successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error"
+        })
+        
+    }
+})
+
 app.listen(8000);
 
 module.exports = app;
