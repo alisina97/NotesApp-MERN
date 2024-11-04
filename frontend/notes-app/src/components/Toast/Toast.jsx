ee45925f8ec379e3183e8e3efa634c6db59b4fdb
Ali Sina <ali.sina1447@gmail.com>
2024-11-04 04:56:45 -0500

    UPDATE: styling Toast component

--- frontend/notes-app/src/components/Toast/Toast.jsx
+++ frontend/notes-app/src/components/Toast/Toast.jsx
@@ -3,8 +3,20 @@ import {LuCheck} from "react-icons/lu";
 
 function Toast({isShown, message, type, onClose}) {
   return (
-    <div>
-      Toast
+    <div className={`absolute top-20 right-6 transition-all duration-400 ${
+      !isShown? "opacity-100" : "opacity-0"
+    }`}>
+      <div className={`min-w-52 bg-white border shadow-2xl rounded-md after:w-[5px] after:h-full ${
+        type === "delete" ? "after:bg-red-500" : "after:bg-green-500"
+      } after:absolute after:left-0 after:top-0 after:rounded-l-lg`}>
+
+        <div className='flex items-center gap-3 py-2 px-4'>
+          <div className={`w-10 h-10 flex items-center justify-center rounded-full ${type === "delete" ? "bg-red-50" : "bg-green-50"}`}>
+            <LuCheck className='text-xl text-green-500'/>
+          </div>
+          <p className='text-sm text-slate-800'>Note added successfully.</p>
+        </div>
+      </div>
     </div>
   )
 }
